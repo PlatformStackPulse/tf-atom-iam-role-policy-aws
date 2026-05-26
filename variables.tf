c06e9f28c204914859215375c5f53b1e88a3b51f
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 20:36:23 +0100

    feat: implement tf-atom-iam-role-policy-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,17 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "role_name" {
+  description = "Name of the IAM role to embed the inline policy in"
+  type        = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9+=,.@_-]+$", var.role_name))
+    error_message = "role_name must contain only valid IAM role name characters."
+  }
+}
+
+variable "policy" {
+  description = "JSON-encoded IAM policy document"
+  type        = string
+  validation {
+    condition     = can(jsondecode(var.policy))
+    error_message = "policy must be valid JSON."
+  }
+}
